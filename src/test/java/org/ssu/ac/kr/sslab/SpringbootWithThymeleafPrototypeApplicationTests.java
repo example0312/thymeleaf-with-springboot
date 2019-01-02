30afa1bcde404ddefc16833e949ede4c8e62a3e2
Jaehyung Ye <example0312@gmail.com>
2019-01-02 16:23:12 +0900

    Add a spring-boot project with spring-web dependency

---
+++ src/test/java/org/ssu/ac/kr/sslab/SpringbootWithThymeleafPrototypeApplicationTests.java
@@ -0,0 +1,17 @@
+package org.ssu.ac.kr.sslab;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootWithThymeleafPrototypeApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
