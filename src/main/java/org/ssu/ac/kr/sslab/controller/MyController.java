ebe54bcce54035e5f25dd7e8c322577cd1d546c8
Jaehyung Ye <example0312@gmail.com>
2019-01-02 16:49:08 +0900

    Change @RestController to @Controller

--- src/main/java/org/ssu/ac/kr/sslab/controller/MyController.java
+++ src/main/java/org/ssu/ac/kr/sslab/controller/MyController.java
@@ -1,11 +1,11 @@
 package org.ssu.ac.kr.sslab.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class MyController {
     @GetMapping("/hello")
     public String hello(Model model, @RequestParam(value="name", defaultValue="World") String name) {
